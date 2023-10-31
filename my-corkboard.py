fabdfbc9899b653e2ea8988fefb85fd88c231069
Mr. Devet <casey.devet@sccdsb.net>
2023-10-31 08:29:14 -0400

    Changed screen dimensions.

--- my-corkboard.py
+++ my-corkboard.py
@@ -8,9 +8,8 @@ from pygame_grid import draw_grid
 pygame.init()
 
 # Create and open a pygame screen with the given size
-width = 640
-height = 360
-
+width = 533
+height = 355
 screen = pygame.display.set_mode((width, height))
 
 # Set the title of the pygame screen
